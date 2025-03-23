177c7a54eb25b9efdd9ae1c5da130ffc926d344f
JulianMella <julianmella2@gmail.com>
2025-03-23 23:20:48 +0100

    Storing program state with @State for simple properties that are stored in one view

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,19 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
     
     var body: some View {
-        NavigationStack {
-            Form {
-                Section {
-                    Text("Within section 1")
-                }
-            }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
+        Button("Tap Count: \(tapCount)") {
+            self.tapCount += 1
         }
     }
-    
 }
 
 #Preview {
