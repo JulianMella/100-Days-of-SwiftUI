72e578c97e3edfe1dd634b7e839077bf4a51c9bb
JulianMella <julianmella2@gmail.com>
2025-03-24 14:54:03 +0100

    Showing alert and messages

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,33 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        /*Button("Delete selection") {
-            print("Now deleting...")
-        }*/
-        /*Button("Delete selection", role: .destructive, action: executeDelete)*/
-        /*VStack {
-            Button("Button 1"){ }.buttonStyle(.bordered)
-            Button("Button 2", role: .destructive) { }.buttonStyle(.bordered)
-            Button("Button 3") { }.buttonStyle(.borderedProminent)
-                .tint(.mint)
-            Button("Button 4", role: .destructive) { }.buttonStyle(.borderedProminent)
-        }*/
-        /*Button("Edit", systemImage: "pencil") {
-            print ("Editing...")
-        }*/
-        /*Button {
-            print("Edit button was tapped")
-        } label: {
-            Label("Edit", systemImage: "pencil")
-                .padding()
-                .foregroundStyle(.white)
-                .background(.red)
-        }*/
-    }
+    @State private var showingAlert = false
     
-    func executeDelete() {
-        print("Now deleting...")
+    var body: some View {
+        Button("Show Alert") {
+            showingAlert = true
+        }
+        
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("Delete", role: .destructive) {}
+            Button("Cancel", role: .cancel) {}
+        } message: {
+            Text("Please read this.")
+        }
     }
 }
 
