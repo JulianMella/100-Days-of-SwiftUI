601468a95f76896938e0ea422ed0b89fe105f57e
JulianMella <julianmella2@gmail.com>
2025-03-24 05:21:43 +0100

    HVZStack

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 20) {
+            Spacer()
             Text("Hello, world!")
-            Text("This is inside a stack")
+            Text("This is inside a vstack")
+            Spacer()
+            Spacer()
+        }
+        .padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0))
+        .border(Color.blue)
+        HStack(spacing: 20) {
+            Text("Hello, world!")
+            Text("This is inside a hstack")
+        }
+        .border(Color.red)
+        ZStack(alignment: .top) {
+            Text("Hello, world!")
+            Text("This is inside a zstack")
         }
     }
 }
