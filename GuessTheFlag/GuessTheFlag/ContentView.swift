3a6f70295bca114dea5bf27a3358e8b567ef3e8d
JulianMella <julianmella2@gmail.com>
2025-03-24 05:27:46 +0100

    3x3 grid with foreach init

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,23 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Spacer()
-            Text("Hello, world!")
-            Text("This is inside a vstack")
-            Spacer()
-            Spacer()
-        }
-        .padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0))
-        .border(Color.blue)
-        HStack(spacing: 20) {
-            Text("Hello, world!")
-            Text("This is inside a hstack")
-        }
-        .border(Color.red)
-        ZStack(alignment: .top) {
-            Text("Hello, world!")
-            Text("This is inside a zstack")
+        VStack {
+            ForEach(0..<3) { outer in
+                HStack {
+                    ForEach(0..<3) { inner in
+                        Text("\(outer * 3 + inner)")
+                    }
+                }
+            }
         }
     }
 }
