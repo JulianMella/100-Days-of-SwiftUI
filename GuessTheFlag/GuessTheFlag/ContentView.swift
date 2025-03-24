34d1c9a3ab2b5145dcd47145cd9d8ce5db2bbb89
JulianMella <julianmella2@gmail.com>
2025-03-24 14:50:06 +0100

    Buttons and images

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,28 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        
-        /*ZStack {
-            Color.red
-            
-            Color(red: 1, green: 0.8, blue: 0)
-                //.frame(width: 200, height: 200)
-                .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
-            Text("Your content")
+        /*Button("Delete selection") {
+            print("Now deleting...")
         }*/
-        //.ignoresSafeArea()
-        ZStack {
-            VStack(spacing: 0) {
-                Color.red
-                Color.blue
-            }
-            
-            Text("Your content")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
-        }
+        /*Button("Delete selection", role: .destructive, action: executeDelete)*/
+        /*VStack {
+            Button("Button 1"){ }.buttonStyle(.bordered)
+            Button("Button 2", role: .destructive) { }.buttonStyle(.bordered)
+            Button("Button 3") { }.buttonStyle(.borderedProminent)
+                .tint(.mint)
+            Button("Button 4", role: .destructive) { }.buttonStyle(.borderedProminent)
+        }*/
+        /*Button("Edit", systemImage: "pencil") {
+            print ("Editing...")
+        }*/
+        /*Button {
+            print("Edit button was tapped")
+        } label: {
+            Label("Edit", systemImage: "pencil")
+                .padding()
+                .foregroundStyle(.white)
+                .background(.red)
+        }*/
+    }
+    
+    func executeDelete() {
+        print("Now deleting...")
     }
 }
 
